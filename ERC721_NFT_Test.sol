2ee92ebba0a1961d7c5f4a7b62cb8384fab606d4
Mariana Marques <65464431+mmvrstudio@users.noreply.github.com>
2022-04-14 20:18:57 -0400

    Add files via upload

---
+++ ERC721_NFT_Test.sol
@@ -0,0 +1,87 @@
+pragma solidity ^0.8.4;
+
+contract AlienOverlord is ERC721, ERC721Enumerable, Pausable, Ownable {
+
+    // ===== 1. 'AlienOverlord' Property Variables ===== //
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    uint256 public MINT_PRICE = 0.00033 eth;
+    uint public MAX_SUPPLY = 10;
+
+    // ===== 2. Lifecycle Methods ===== //
+
+    constructor() ERC721("AlienOverlord", "AO") {
+        // Start token ID at 1. By default is starts at 0.
+        _tokenIdCounter.increment();
+    }
+    function withdraw() public onlyOwner() {
+        require(address(this).balance > 0, "Balance is zero");
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    // ===== 3. 'AlienOverlord' Pauseable Functions ===== //
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // ===== 4. 'AlienOverlord' Minting Functions ===== //
+
+    function safeMint(address to) public payable {
+        // Note: Check that totalSupply is less than MAX_SUPPLY
+        require(totalSupply() < MAX_SUPPLY, "Can't mint anymore tokens.");
+
+        // Note 2: Check if ether value is correct
+        require(msg.value >= MINT_PRICE, "Not enough ether sent.");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    // ===== 5. Other Functions ===== //
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://alienoverlordBaseURI/";
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
+
+/* IMPORTANT NOTES:
+
+    contract address: 0xd9145CCE52D386f254917e481eB44e9943F39138
+    mint price: 0.00033 eth
+
+    // Users
+    owner: 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+        - deployed contract
+        - can only call the `onlyOwner` modifier functions
+
+    address 2: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+        - mint 1 NFT
+
+    address 3: 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
+        - address 2 will transfer NFT #1 to address 3 (recipient)
+
+*/
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# SOLIDITY_Test_ERC721
+# NFT Test | ERC-721 Solidity
+- Select ERC721
+- Change Name and Symbol
+- Edit Base URI - for your tokens
 
+Code Sections:
+  - 1. Property Variables
+  - 2. Lifecycle Methods
+  - 3. Pauseable Functions
+  - 4. Minting Functions
+
+### Necessary Modifications
+- Remove `onlyOwner` modifier from `safeMint`
+- Add in `MINT_PRICE` in ether. 
+- Add corresponding `require` statement in `safeMint()
+- Add `MAX_SUPPLY` for total number of tokens. 
+- Add corresponding `require` statement in `safeMint()`
+- In the constructor, call `_tokenIdCounter.increment()` to start the token at 1
+- Add in `withdraw()` function
+
+## 3. Deployment Instructions:
+- Check compiler version at the type of the Solidity File:
+  ex: `pragma solidity ^0.8.4;`
+- Select the compiler from the left hand side of the page and select the corresponding compiler version - 0.8.4 or whatever the newest version is out.
+- Compile the contract
+- Select the `Deploy & Run Transactions` tab.
+  Environment: JavaScript VM
+  Account: Make note of what account you deploy it on
+  Contract: Select `YourContract.sol` from the bottom of the dropdown list.
+- View the deployed contract and examine transactions.
